2303990c6c62cc97b538c324e3a65dfc6f3cf11b
Prem Chandrasekaran <premanandc@gmail.com>
2020-01-15 06:30:15 -0500

    Simplified the contract

--- customer-client/src/test/java/com/cinjug/customer/client/CustomerClientContractTests.java
+++ customer-client/src/test/java/com/cinjug/customer/client/CustomerClientContractTests.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.Collection;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.groups.Tuple.tuple;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.NONE;
 import static org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties.StubsMode.LOCAL;
 
@@ -28,7 +29,13 @@ public class CustomerClientContractTests {
 
     Collection<Customer> customers = client.findAll();
 
-    assertThat(customers).hasSize(3);
+    assertThat(customers).hasSize(3)
+            .extracting(Customer::getFirstName, Customer::getLastName, Customer::getEmail)
+            .containsExactly(
+                    tuple("First1", "Last1", "first1@test.com"),
+                    tuple("First2", "Last2", "first2@test.com"),
+                    tuple("First3", "Last3", "first3@test.com")
+            );
   }
 
 }
--- customer-service/src/test/resources/contracts/webClient/shouldGetAllCustomers.groovy
+++ customer-service/src/test/resources/contracts/webClient/shouldGetAllCustomers.groovy
@@ -3,7 +3,7 @@ package contracts.webClient
 import org.springframework.cloud.contract.spec.Contract
 
 Contract.make {
-  description("""
+    description("""
 Represents a successful scenario to get all customers
 
 ```
@@ -13,27 +13,36 @@ when:
     a request to get all customers
 then:
     an array of customers should be returned
-
 ```
 """)
-  request {
-    method 'GET'
-    url '/customers'
-  }
-  response {
-    status OK()
-    headers {
-      contentType('application/json')
+    request {
+        method 'GET'
+        url '/customers'
     }
-    body(
-        (1..3).collect {
-          [
-              id       : $(producer(anyNumber())),
-              firstName: $(consumer("First$it"), producer(regex('[\\w\\d]+'))),
-              lastName : $(consumer("Last$it"), producer(regex('[\\w\\d]+'))),
-              email    : $(consumer("first$it@test.com"), producer(regex('^\\S+@\\S+$')))
-          ]
+    response {
+        status OK()
+        headers {
+            contentType('application/json')
         }
-    )
-  }
+        body([
+                [
+                        id       : 1,
+                        firstName: 'First1',
+                        lastName : 'Last1',
+                        email    : 'first1@test.com'
+                ],
+                [
+                        id       : 2,
+                        firstName: 'First2',
+                        lastName : 'Last2',
+                        email    : 'first2@test.com'
+                ],
+                [
+                        id       : 3,
+                        firstName: 'First3',
+                        lastName : 'Last3',
+                        email    : 'first3@test.com'
+                ]
+        ])
+    }
 }
